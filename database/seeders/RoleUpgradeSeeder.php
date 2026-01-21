833875709930efd7b1557c63beffc38e08320bf0
juniyasyos <juniyasyos@gmail.com>
2026-01-21 21:30:12 +0700

    feat: update use statements in RoleUpgradeSeeder for consistency

--- database/seeders/RoleUpgradeSeeder.php
+++ database/seeders/RoleUpgradeSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Role;
 use App\Models\User;
-use BezhanSalleh\FilamentShield\Support\Utils;
+use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 
 class RoleUpgradeSeeder extends Seeder
