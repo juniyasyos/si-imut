77c58329170be16f501cf6605c87826bc7d866a9
juniyasyos <juniyasyos@gmail.com>
2026-01-22 02:17:58 +0700

    feat: reorder use statements in RoleUpgradeSeeder for consistency

--- database/seeders/RoleUpgradeSeeder.php
+++ database/seeders/RoleUpgradeSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 
 class RoleUpgradeSeeder extends Seeder
