a408606c2568ea7c61277e83200f469c3159048f
juniyasyos <juniyasyos@gmail.com>
2026-03-10 13:02:53 +0700

    fix: Disable activity logging by default in configuration

--- app/Filament/Resources/ActivitylogResource.php
+++ app/Filament/Resources/ActivitylogResource.php
@@ -9,7 +9,14 @@ use Rmsramos\Activitylog\Resources\ActivitylogResource as BaseActivitylogResourc
 class ActivitylogResource extends BaseActivitylogResource implements HasShieldPermissions
 {
     use HasActiveIcon;
+
     protected static ?int $navigationSort = 3;
+
+    public static function shouldRegisterNavigation(): bool
+    {
+        return false;
+    }
+
     public static function getPermissionPrefixes(): array
     {
         return [
