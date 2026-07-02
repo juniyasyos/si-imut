65887207a2f07f6cc632ba2032575381e8be526e
juniyasyos <juniyasyos@gmail.com>
2026-07-02 19:05:44 +0700

    fix: chanfe version 2.0.0 to 2.1.0 in global footer

--- resources/views/filament/global-footer.blade.php
+++ resources/views/filament/global-footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="w-full mt-auto py-4 text-center">
     <div class="text-sm text-gray-500 dark:text-gray-400">
-        &copy; {{ date('Y') }} SIIMUT. All rights reserved. <span class="ml-2 font-medium">v2.0.0</span>
+        &copy; {{ date('Y') }} SIIMUT. All rights reserved. <span class="ml-2 font-medium">v2.1.0</span>
     </div>
 </footer>
